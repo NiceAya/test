68562dd37c31eadf8664e8a214234bc87988bf6b
niceAya <2232995265@qq.com>
2023-03-29 17:29:46 +0800

    create xcx new file

---
+++ unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -0,0 +1,33 @@
+"use strict";
+var __defProp = Object.defineProperty;
+var __getOwnPropSymbols = Object.getOwnPropertySymbols;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __propIsEnum = Object.prototype.propertyIsEnumerable;
+var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
+var __spreadValues = (a, b) => {
+  for (var prop in b || (b = {}))
+    if (__hasOwnProp.call(b, prop))
+      __defNormalProp(a, prop, b[prop]);
+  if (__getOwnPropSymbols)
+    for (var prop of __getOwnPropSymbols(b)) {
+      if (__propIsEnum.call(b, prop))
+        __defNormalProp(a, prop, b[prop]);
+    }
+  return a;
+};
+var common_vendor = require("../../common/vendor.js");
+const _sfc_main = {
+  setup() {
+    let data = common_vendor.reactive({
+      title: "\u6807\u98981111"
+    });
+    return __spreadValues({}, common_vendor.toRefs(data));
+  }
+};
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return {
+    a: common_vendor.t(_ctx.title)
+  };
+}
+var MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "E:/\u878D\u5A92/\u610F\u5B66\u5802\u62DB\u5546/XCX2.0/pages/index/index.vue"]]);
+wx.createPage(MiniProgramPage);
